58b29460bcb96db6c91c1b80705fa00d414b51a6
dexy1024 <dexy1024@users.noreply.github.com>
2025-11-06 21:18:15 +0800

    配置GitHub Pages部署

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/dashboard_of_mxbc/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/dashboard_for_mxbc/' : '/',
   build: {
     outDir: 'dist'
   }
